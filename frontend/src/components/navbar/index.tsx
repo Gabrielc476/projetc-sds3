6878bd124dac245236c32cf250751b32726249fd
Gabriel Castro <gabrielc.cavalcante@hotmail.com>
2021-09-07 23:24:39 -0300

    correção

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,11 +1,11 @@
 
-import ImgDsDark from "assets/img/converted-ds-dark.jpg";
+
 const NavBar = () =>{
     return(
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
           <div className = "container">
               <nav className = "my-2 my-md-0 mr-md-3">
-                  <img src="{ImgDsDark}" alt="DevSuperior" width="120"/>
+                  <img src="./assets/img/converted-ds-dark.jpg" alt="DevSuperior" width="120"/>
               </nav>
             </div>  
         </div>
